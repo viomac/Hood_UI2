cbc7d95943114f78f973ffd5d3079fa7775ce93d
viomac <vmaceda29@gmail.com>
2015-06-29 19:10:59 -0500

    Updated UIs
    Added icons, styles and fragments files for login, signup and main

--- app/src/main/java/com/example/richit/hood_ui/MainFragment.java
+++ app/src/main/java/com/example/richit/hood_ui/MainFragment.java
@@ -18,6 +18,6 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        return inflater.inflate(R.layout.fragment_login, container, false);
     }
 }
